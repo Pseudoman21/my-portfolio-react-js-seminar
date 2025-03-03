8d0c9c919f88c4c618929ef5de87e09bcdb8d0e7
Pseudoman21 <johnpaul@meddicc.com>
2025-03-03 11:01:16 +0800

    Update

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,7 +4,7 @@ import "./index.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Portfolio SPA",
+  title: "John Paul Batusan Portfolio",
   description: "A single page application portfolio with smooth scrolling",
 }
 
